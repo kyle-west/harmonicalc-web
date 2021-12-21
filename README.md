399c33db44313d1f546c4a0955be555689008ebd
Kyle West <kyle@familysearch.org>
2021-12-21 14:31:18 -0700

    [calc] added functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+# HarmoniCalc Web
+
+This is the web version of my mixed-base calculator app.
+
+![HarmoniCalc](./HarmoniCalc.png)
+
+I built this mostly as a baseline for the Swift and Kotlin versions to allow me to have something to work with as I learn those languages. 
+
+Built this in a day. Not sure what bugs there are. Let me know in an issue or on twitter if you encounter a problem.
+
+**Built with ❤️ by [@KyleWestCS](https://twitter.com/KyleWestCS)**
---
+++ view.js
@@ -0,0 +1,87 @@
+const queryButtons = (query = '') => [...document.querySelectorAll(`.buttons button${query}`)]
+const buttons = Object.fromEntries(queryButtons().map(btn => [btn.id, btn]));
+const omnibox = document.getElementById('omnibox');
+
+const strictOperators = ['&', '|', '^', '%', '/', '*', '+', '-'];
+let lastItemWasResult = false
+let cursorPosition = 0;
+
+function clear (reset = '') {
+  omnibox.value = reset;
+  lastItemWasResult = false;
+  cursorPosition = reset.length;
+}
+
+function execute () {
+  if (!omnibox.value) return;
+  let result;
+  try {
+    console.log(omnibox.value);
+    result = eval(omnibox.value);
+  } catch (err) {
+    console.error(err);
+    result = NaN;
+  } finally {
+    lastItemWasResult = true;
+    omnibox.value = result;
+  }
+}
+
+function autoClearAfterExec({ data, currentTarget }) {
+  if (lastItemWasResult && !strictOperators.includes(data)) {
+    clear(currentTarget ? data : '');
+  }
+  lastItemWasResult = false;
+}
+
+function handleCursor({ currentTarget: field }) {
+  cursorPosition = field.selectionStart;
+  console.log(cursorPosition);
+}
+
+function typeChar (text) {
+  return () => {
+    autoClearAfterExec({ data: text });
+    omnibox.value = omnibox.value.slice(0, cursorPosition) + text + omnibox.value.slice(cursorPosition);
+    cursorPosition += text.length;
+    applyPrefixes();
+  }
+}
+
+function ensureHexPrefixIfNeeded(word) {
+  if (/[a-fA-F]+$/.test(word) && !word.startsWith('0x') && !word.startsWith('0b')) {
+    cursorPosition += 2;
+    return `0x${word}`;
+  }
+}
+
+function applyPrefixes () {
+  omnibox.value = omnibox.value.replace(/\w+/g, (word) => {
+    return ensureHexPrefixIfNeeded(word) || word;
+  })
+}
+
+function applySideEffects(evt) {
+  handleCursor(evt);
+  autoClearAfterExec(evt);
+  applyPrefixes();
+}
+
+buttons.equals.addEventListener('click', execute);
+buttons.clear.addEventListener('click', () => clear());
+queryButtons('[data-value]').forEach(
+  btn => btn.addEventListener('click', typeChar(btn.dataset.value))
+);
+buttons.typeHEX.addEventListener('click', () => {
+  execute()
+  omnibox.value = `0x${Number(omnibox.value).toString(16)}`;
+})
+buttons.typeBIN.addEventListener('click', () => {
+  execute()
+  omnibox.value = `0b${Number(omnibox.value).toString(2)}`;
+})
+buttons.typeDEC.addEventListener('click', () => execute())
+
+omnibox.addEventListener('input', applySideEffects);
+omnibox.addEventListener('keydown', handleCursor);
+omnibox.addEventListener('keyup', ({ key }) => key === 'Enter' && execute());
